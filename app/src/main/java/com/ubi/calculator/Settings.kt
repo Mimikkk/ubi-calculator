d583a994b7b860347f0764c7060a9e8e55fa3dd1
Hououin Kyouma <inwazzin@gmail.com>
2022-04-06 19:27:19 +0200

    settings layout

---
+++ app/src/main/java/com/ubi/calculator/Settings.kt
@@ -0,0 +1,3 @@
+package com.ubi.calculator
+
+data class Settings(var screenColor: String? = "green", var numberPrecision: Int = 2)
--- app/src/main/java/com/ubi/calculator/SettingsLayout.kt
+++ app/src/main/java/com/ubi/calculator/SettingsLayout.kt
@@ -2,10 +2,14 @@ package com.ubi.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ubi.calculator.databinding.SettingsLayoutBinding
 
 class SettingsLayout : AppCompatActivity() {
+  private lateinit var binding: SettingsLayoutBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.settings_layout)
+    binding = SettingsLayoutBinding.inflate(layoutInflater)
   }
 }
