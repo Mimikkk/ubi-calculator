4b61625dc329f44b5cea4bdf20c396666978411d
Hououin Kyouma <inwazzin@gmail.com>
2022-04-06 19:01:39 +0200

    settings layout

---
+++ app/src/main/java/com/ubi/calculator/SettingsLayout.kt
@@ -0,0 +1,11 @@
+package com.ubi.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsLayout : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.settings_layout)
+  }
+}
